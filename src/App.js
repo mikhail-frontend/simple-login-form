c8e5c3af7fb46da7477af275eb552cb4312db723
mikhail-frontend <mikhail.kharitonov2022.frontend@gmail.com>
2022-10-15 17:25:07 +0300

    cleaning code a bit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
 import AuthContext from "./context/auth-context";
 
-function App() {
+const App = () => {
     const authCtx = useContext(AuthContext)
     return (
         <>
